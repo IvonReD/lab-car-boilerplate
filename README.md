4b5be6768aba6c64bc4c53d9d25a9645a082781a
IvonReD <ivonne.reyes.dc@gmail.com>
2018-01-14 21:43:21 -0600

    Subiendo reto Labcar

---
+++ README.md
@@ -0,0 +1,9 @@
+#LAB-<AR
+
+Recrea la siguiente página web. Recuerda hacer un fork del repositorio y clonarlo en tu máquina.
+
+[Pantalla 1](./assets/images/pantalla.jpg)
+
+[Pantalla 2](./assets/images/pantalla2.jpg)
+
+[Pantalla 3](./assets/images/pantalla3.jpg)
--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-// La funcionalidad de tu proyecto
+function initMap() {
+  var uluru = {lat:23.0888496, lng: -100.2951916};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
